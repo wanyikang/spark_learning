da379abeda594cabc28dc7be2264f419f68ae73f
richard_wan <richard_wan@localhost.localdomain>
2018-06-28 14:24:06 +0800

    add simple app of spark

---
+++ simple_app.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from pyspark.sql import SparkSession
+from os.path import expanduser, join
+
+logFile = '/home/richard_wan/Downloads/spark-2.3.0-bin-hadoop2.7/README.md'
+spark = SparkSession.builder.appName('SimpleApp').getOrCreate()
+logData = spark.read.text(logFile).cache()
+
+numAs = logData.filter(logData.value.contains('a')).count()
+numBs = logData.filter(logData.value.contains('b')).count()
+
+print('Lines with a: %i, lines with b: %i' % (numAs, numBs))
+
+spark.stop()
+
